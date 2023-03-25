e7444d70df7f9579ab34688bd639a9e9992be2ae
TheStormex <55155047+TheStormex@users.noreply.github.com>
2023-03-24 23:32:45 -0400

    video works

--- js/script.js
+++ js/script.js
@@ -3,21 +3,38 @@
 let darkMode = false;
 // is the page a project or the main page?
 let isProject = false;
-let currentSection = "";
+let videoPlaying = false;
+let currentSection;
+
 let projectPages = [];
 let mainPage;
 let projectPage;
 
-function toProjectPage() {
+function toProjectPage(mainSection, projectName) {
   mainPage = document.getElementById('mainPage');
   projectPage = document.getElementById('projectPage');
-  mainPage.style.display = "none";
-  projectPage.style.display = "inline-block";
+  $(mainPage).fadeOut(150);
+  $(projectPage).fadeIn(150);
 }
 
 function toMainPage() {
   mainPage = document.getElementById('mainPage');
   projectPage = document.getElementById('projectPage');
-  projectPage.style.display = "none";
-  mainPage.style.display = "inline-block";
+  $(projectPage).fadeOut(150);
+  $(mainPage).fadeIn(150);
+}
+
+function playVideo() {
+  $('#video').fadeOut(100);
+  $('#overlay').fadeOut(100);
+  $('#videoFull').fadeIn(100);
+  document.getElementById('videoFull').addEventListener('ended', finishVideo, false);
+  videoPlaying = true;
+}
+
+function finishVideo() {
+  videoPlaying = false;
+  $('#videoFull').fadeOut(0);
+  $('#video').fadeIn(0);
+  $('#overlay').fadeIn(0);
 }
